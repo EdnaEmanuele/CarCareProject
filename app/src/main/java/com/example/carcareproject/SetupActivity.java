e9e2be0d3c64ec8969819dfb9e1911c85533cdc6
EdnaEmanuele <87340605+EdnaEmanuele@users.noreply.github.com>
2024-01-15 19:01:31 +0000

    0115

--- app/src/main/java/com/example/carcareproject/SetupActivity.java
+++ app/src/main/java/com/example/carcareproject/SetupActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +21,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 
 import java.net.URI;
 import java.util.HashMap;
@@ -33,8 +38,10 @@ public class SetupActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference UserRef;
     private ProgressDialog loadingBar;
+    private StorageReference UserProfileImageRef;
     String currentUserID;
     final static int Gallery_Pick = 1;
+    private Activity activity;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +51,7 @@ public class SetupActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         currentUserID = mAuth.getCurrentUser().getUid();
         UserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID);
+        UserProfileImageRef = FirebaseStorage.getInstance().getReference().child("Profile Image");
 
         CompanyName = (EditText) findViewById(R.id.setup_company_name);
         UserName = (EditText) findViewById(R.id.setup_user_name);
@@ -63,14 +71,35 @@ public class SetupActivity extends AppCompatActivity {
         ProfileImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) { //To choose the picture from the gallery
-                Intent galleryIntent = new Intent();
-                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType("image/*");
-                startActivityForResult(galleryIntent, Gallery_Pick);
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(Intent.createChooser(galleryIntent, "Choose your profile image"), 1);
             }
         });
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK && requestCode == Gallery_Pick) {
+            Uri selectedImageUri = data.getData();
+            ProfileImage.setImageURI(selectedImageUri);
+
+            if (selectedImageUri != null) {
+                StorageReference filePath = UserProfileImageRef.child(currentUserID + ".jpg");
+                filePath.putFile(selectedImageUri).addOnSuccessListener(taskSnapshot -> {
+                            // Imagem enviada com sucesso
+                            Toast.makeText(SetupActivity.this, "Profile image stored successfully", Toast.LENGTH_SHORT).show();
+                            filePath.getDownloadUrl().addOnSuccessListener(uri -> {
+                                String downloadUrl = uri.toString();
+                            });
+                        })
+                        .addOnFailureListener(e -> {
+                            // Ocorreu um erro ao enviar a imagem
+                            Toast.makeText(SetupActivity.this, "Error uploading image: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        });
+            }
+        }
+    }
 
 
 
