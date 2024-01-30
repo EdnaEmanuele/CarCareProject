df83786de574c1dd3534348409f32b56612047b7
EdnaEmanuele <87340605+EdnaEmanuele@users.noreply.github.com>
2024-01-30 21:29:42 +0000

    30/01 .2 postActivity

--- app/src/main/java/com/example/carcareproject/PostActivity.java
+++ app/src/main/java/com/example/carcareproject/PostActivity.java
@@ -1,11 +1,16 @@
 package com.example.carcareproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.core.content.ContextCompat;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.Toast;
-
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.MenuItem;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -18,5 +23,32 @@ public class PostActivity extends AppCompatActivity {
 
         mtoolbar =(Toolbar) findViewById(R.id.update_post_toolbar);
         setSupportActionBar(mtoolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.seta_atras);
+
+        // Defina a cor desejada
+        int corDoTexto = ContextCompat.getColor(this, R.color.White);
+
+        // Defina a cor do texto "Update post"
+        String title = "Update post";
+        Spannable text = new SpannableString(title);
+        text.setSpan(new ForegroundColorSpan(corDoTexto), 0, text.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        getSupportActionBar().setTitle(text);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home){
+            SendUserToMainActivity();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(PostActivity.this, MainActivity.class);
+        startActivity(mainIntent);
     }
 }
